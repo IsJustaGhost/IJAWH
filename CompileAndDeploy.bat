aaf8cd89dbe2116bd8d40d9b9ac67b03ce2134ce
IsJustaGhost <56104601+IsJustaGhost@users.noreply.github.com>
2021-10-19 05:24:25 -0700

    Delete CompileAndDeploy.bat

--- CompileAndDeploy.bat
+++
@@ -1,5 +0,0 @@
-:: This script will
-::   - Create a .zip-file containing all addon files
-::   - Copy the addon to ESO's live\AddOn folder
-:: @PowerShell.exe -NoExit -ExecutionPolicy Bypass -File "..\Framework\PowerShell\CompileAndDeploy.ps1" "%cd%"
-@PowerShell.exe -ExecutionPolicy Bypass -File "..\Framework\PowerShell\CompileAndDeploy.ps1" "%cd%"
